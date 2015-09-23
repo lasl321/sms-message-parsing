c08fe916e73a0a3bf02fd1bf34a8e7f41752ca17
Luis Sotomayor <LSotomayor@futuredontics.com>
2015-09-23 12:29:18 -0700

    Added tests to try to figure out issue

---
+++ sms-message-parsing/Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+
+namespace Spikes.SMSMessageParsing
+{
+    public class Tests
+    {
+        private const string Message = "abcdefghijklmnopqrstuvwxyz";
+
+        [Test]
+        public void ShouldEncode()
+        {
+            var bytes = Utilities.GetMessageBytes(Message, Encoding.ASCII);
+            Utilities.PrintBytes("ASCII", bytes);
+            bytes = Utilities.GetMessageBytes(Message, Encoding.UTF8);
+            Utilities.PrintBytes("UTF8", bytes);
+            bytes = Utilities.GetMessageBytes(Message, Encoding.GetEncoding("iso-8859-1"));
+            Utilities.PrintBytes("Latin 1", bytes);
+            bytes = Utilities.GetMessageBytes(Message, Encoding.Unicode);
+            Utilities.PrintBytes("Unicode", bytes);
+            bytes = Utilities.GetMessageBytes(Message, Encoding.BigEndianUnicode);
+            Utilities.PrintBytes("Big End", bytes);
+        }
+
+        [Test]
+        public void ShouldDecodeInUtf8FromLatin1()
+        {
+            var bytes = Utilities.GetMessageBytes(Message, Encoding.GetEncoding("iso-8859-1"));
+            using (var reader = new StreamReader(new MemoryStream(bytes)))
+            {
+                var contents = reader.ReadToEnd();
+                Console.WriteLine(contents);
+                CreateXmlDocument(contents);
+            }
+        }
+
+        [Test]
+        public void ShouldDecodeInUtf8FromUnicode()
+        {
+            var bytes = Utilities.GetMessageBytes(Message, Encoding.Unicode);
+            using (var reader = new StreamReader(new MemoryStream(bytes)))
+            {
+                var contents = reader.ReadToEnd();
+                Console.WriteLine(contents);
+                CreateXmlDocument(contents);
+            }
+        }
+
+        [Test]
+        public void ShouldDecodeInUtf8FromBigEndianUnicode()
+        {
+            var bytes = Utilities.GetMessageBytes(Message, Encoding.BigEndianUnicode);
+            using (var reader = new StreamReader(new MemoryStream(bytes)))
+            {
+                var contents = reader.ReadToEnd();
+                Console.WriteLine(contents);
+                CreateXmlDocument(contents);
+            }
+        }
+
+        private static void CreateXmlDocument(string contents)
+        {
+            string xml = $"<?xml version=\"1.0\"?><foo>{contents}</foo>";
+            Console.WriteLine($"XML: {xml}");
+            var document = new XmlDocument();
+            document.LoadXml(xml);
+        }
+    }
+}
---
+++ sms-message-parsing/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Spikes.SMSMessageParsing
+{
+    internal static class Utilities
+    {
+        public static byte[] GetMessageBytes(string message, Encoding encoding)
+        {
+            var stream = new MemoryStream();
+            using (var writer = new StreamWriter(stream, encoding))
+            {
+                writer.Write(message);
+            }
+
+            return stream.ToArray();
+        }
+
+        public static void PrintBytes(string encodingName, IEnumerable<byte> messageBytes)
+        {
+            Console.Write("{0,-10}", encodingName);
+            foreach (var b in messageBytes)
+            {
+                Console.Write("{0:x} ", b);
+            }
+            Console.WriteLine();
+        }
+    }
+}
