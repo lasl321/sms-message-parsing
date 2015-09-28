551625bcbe75f8b2b13b16cc8ca4b9f73a68d1bc
Luis Sotomayor <LSotomayor@futuredontics.com>
2015-09-28 13:35:31 -0700

    Added some tests for Emojis

---
+++ sms-message-parsing/EmojiTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace Spikes.SMSMessageParsing
+{
+    public class EmojiTests
+    {
+        [Test]
+        public void ShouldPrintHappyFace()
+        {
+            var bytes = new byte[]
+            {
+                0xF0,
+                0x9F,
+                0x98,
+                0x81
+            };
+
+            var stream = new MemoryStream(bytes);
+            using (var reader = new StreamReader(stream))
+            {
+                var s = reader.ReadToEnd();
+                Console.WriteLine(s);
+            }
+        }
+
+
+        [Test]
+        public void ShouldPrintCatFace()
+        {
+            var bytes = new byte[]
+            {
+                0xF0,
+                0x9F,
+                0x98,
+                0xBC
+            };
+
+            var stream = new MemoryStream(bytes);
+            using (var reader = new StreamReader(stream))
+            {
+                var s = reader.ReadToEnd();
+                Console.WriteLine(s);
+            }
+        }
+
+
+        [Test]
+        public void ShouldPrintC()
+        {
+            var bytes = new byte[]
+            {
+                0x00,
+                0x43
+            };
+
+            var stream = new MemoryStream(bytes);
+            using (var reader = new StreamReader(stream, Encoding.BigEndianUnicode))
+            {
+                var s = reader.ReadToEnd();
+                Console.WriteLine("'{0}'", s);
+            }
+        }
+
+        [Test]
+        public void ShouldPrint()
+        {
+            const string value = "😼";
+
+            var bytes = Utilities.GetMessageBytes(value, Encoding.Unicode);
+            Utilities.PrintBytes("Unicode", bytes);
+
+        }
+    }
+}
